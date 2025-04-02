70a918e36fda929ebcb760a5fb07efed8a07052c
ryanhg <ryan@ryangold.me>
2025-04-02 22:25:29 +0700

    test file update

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
-from symai import Import
+from symai import Import, Symbol
 
-ChonkieChunker = Import.load_expression('ExtensityAI/chonkie-symai', 'ChonkieChunker')
+# ChonkieChunker = Import.load_expression('ExtensityAI/chonkie-symai', 'ChonkieChunker')
+from src.chonkie_chunker import ChonkieChunker
 
 # Initialize chunker and tokenizer
 chunker = ChonkieChunker(
@@ -8,6 +9,6 @@ chunker = ChonkieChunker(
     chunker_name="RecursiveChunker"
 )
 
-print(chunker("Hello, world! This is a test."))
+print(chunker(Symbol("Hello, world! This is a test.")))
 
 
